2d25711c778a686a70507edf0b91afc476fffaad
Gopal <gopalakrishna.gunji@gmail.com>
2026-08-16 02:29:30 -0400

    terraform

--- variables.tf
+++ variables.tf
@@ -28,3 +28,37 @@ variable "public_subnet_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"] 
+}
+variable "private_subnet_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "database_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"] 
+}
+variable "database_subnet_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+
+variable "public_route_table_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "database_route_table_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_route_table_tags" {
+  type        = map(string)
+  default     = {}
+}
